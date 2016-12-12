b18e7a6de5655e8cc67074e8f08f847d4d2740a0
avani19 <avaniipatels5@gmail.com>
2016-12-12 14:09:59 -0500

    want to do more changes

--- public/app.js
+++ public/app.js
@@ -3,9 +3,10 @@ $.getJSON('/articles', function(data) {
   // for each one
   for (var i = 0; i<data.length; i++){
     // display the apropos information on the page
-    $('#articles').append('<div class="col m12 m6">' + '<div class="card blue-grey darken-1">'  + data[i].title + '<br />'+ data[i].link + '</br>' + '<button id="commentBtn" type="submit" data-target="modal1" class="waves-effect waves-light btn teal lighten-1" data-id="' + data[i]._id + '">' + 'Add Comment' + '</button>' +'</div>' + '</div>');
+    $('#articles').append('<div class="col m12 m6">' + '<div class="card blue-grey darken-1">'  + data[i].title + '<br />'+ data[i].link + '</br>' + '<button id="commentBtn" type="submit" data-target="modal1" class="waves-effect waves-light btn teal lighten-1" data-id="' + data[i]._id + '">' + 'Add Comment' + '</button>'+'</div>' + '</div>');
   }
 });
+// '<div class="card-action">' + '<p id="name">' + '</p>' + ' <p id="comm">' + '</p>' + '</div>'+ 
 // whenever someone clicks a p tag
 $(document).on('click', '#commentBtn', function(){
   // empty the comments from the comment section
@@ -23,11 +24,11 @@ $(document).on('click', '#commentBtn', function(){
     .done(function( data ) {
       console.log(data);
       // the title of the article
-      $('.modal-content').append('<h4>' + data.title + '</h4>'); 
+      $('.modal-content').append('<h4>' + "Type your comment" + '</h4>'); 
       // an input to enter a new title
       $('.modal-content').append('<input placeholder="Type your name" id="first_name" type="text" class="validate">'); 
       // a textarea to add a new comment body
-      $('.modal-content').append('<textarea id="bodyinput" name="body"></textarea>'); 
+      $('.modal-content').append('<textarea id="bodyinput" name="body"></textarea>');
       // a button to submit a new comment, with the id of the article saved to it
       $('.modal-footer').append('<button class=" modal-action modal-close waves-effect waves-green btn-flat" data-id="' + data._id + '" id="savecomment">Save Comment</button>');
 
@@ -38,8 +39,8 @@ $(document).on('click', '#commentBtn', function(){
       // if there's a comment in the article
       if(data.comment){
         // place the title of the comment in the title input
-        $('.card-action').append('Name: ' + data.comment.title );
-        $('.card-action').append('Comment: ' + data.comment.body);
+        $('#first_name').val(data.comment.title);
+        $('#bodyinput').val(data.comment.body);
         // $('#first_name').val(data.comment.title);
         // place the body of the comment in the body textarea
         // $('#bodyinput').val(data.comment.body);
